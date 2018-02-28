ea569a679e7f7a7b59b1076d364700de00d40a8c
Gowtham <me@gauti.info>
2018-03-01 12:54:10 +1300

    Not supported page changes

--- pages/browser-not-supported.js
+++ pages/browser-not-supported.js
@@ -6,9 +6,13 @@ import Metas from '~/components/Metas';
 export default () => (
   <div className="not-supported">
     <Metas />
-    <h2>Browser Unsupported</h2>
+    <div className="not-supported__header">
+      <img src="/static/android-chrome-512x512.png" width="100" height="100" />
+      <h1 className="not-supported__logo__text">Compare Movies</h1>
+    </div>
+    <h3>Browser Not Supported</h3>
     <p className="not-supported__paragraph">
-      Please update your browser to run this app.
+      Please update your browser to run this app
     </p>
     <a
       href="https://updatemybrowser.org/"
