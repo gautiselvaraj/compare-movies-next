6dda689e2fe1e0d934589ff817beb79956b1df66
Gowtham <me@gauti.info>
2018-03-01 13:05:13 +1300

    Error page UI

---
+++ pages/_error.js
@@ -0,0 +1,36 @@
+import 'isomorphic-fetch';
+import '~/styles/index.scss';
+import '~/styles/errors.scss';
+
+import React, { Component } from 'react';
+import withRedux from 'next-redux-wrapper';
+import Helmet from 'react-helmet';
+import initStore from '~/store';
+import Layout from '~/components/Layout';
+import Metas from '~/components/Metas';
+
+class ErrorPage extends Component {
+  static getInitialProps({ res, err }) {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : null;
+    return { statusCode };
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Metas />
+        <div className="error">
+          <h3 class="error__heading">
+            {this.props.statusCode === 404 ? (
+              <span>This page could not be found!</span>
+            ) : (
+              <span>Something went wrong</span>
+            )}
+          </h3>
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export default withRedux(initStore)(ErrorPage);
