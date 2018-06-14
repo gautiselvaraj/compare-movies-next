8177ca41222e0816950d8226d5b6bfd50acebe99
Gowtham <me@gauti.info>
2018-06-15 10:46:10 +1200

    Add next compose plugin and cross env
    Refactor how env is handled

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 require('dotenv').config();
-const withSass = require('@zeit/next-sass');
-const webpack = require('webpack');
 
-module.exports = withSass({
-  webpack: config => {
-    config.plugins.push(new webpack.EnvironmentPlugin(process.env));
+const withPlugins = require('next-compose-plugins');
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const withSass = require('@zeit/next-sass');
+
+module.exports = withPlugins([[withSass]], {
+  poweredByHeader: false,
+  webpack: (config, { isServer }) => {
+    config.plugins = config.plugins || [];
+
+    config.plugins = [
+      ...config.plugins,
+      new Dotenv({ path: path.join(__dirname, '.env'), systemvars: true })
+    ];
+
     return config;
   }
 });
