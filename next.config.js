3147ecda51be02092631c2074534f95b5870d0fb
Gowtham <me@gauti.info>
2018-06-15 10:52:34 +1200

    Add some next plugins

--- next.config.js
+++ next.config.js
@@ -4,17 +4,42 @@ const withPlugins = require('next-compose-plugins');
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
 const withSass = require('@zeit/next-sass');
+const withProgressBar = require('next-progressbar');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-module.exports = withPlugins([[withSass]], {
-  poweredByHeader: false,
-  webpack: (config, { isServer }) => {
-    config.plugins = config.plugins || [];
+const { ANALYZE } = process.env;
 
-    config.plugins = [
-      ...config.plugins,
-      new Dotenv({ path: path.join(__dirname, '.env'), systemvars: true })
-    ];
+module.exports = withPlugins(
+  [
+    [withSass],
+    [
+      withProgressBar,
+      {
+        profile: true
+      }
+    ]
+  ],
+  {
+    poweredByHeader: false,
+    webpack: (config, { isServer }) => {
+      config.plugins = config.plugins || [];
 
-    return config;
+      config.plugins = [
+        ...config.plugins,
+        new Dotenv({ path: path.join(__dirname, '.env'), systemvars: true })
+      ];
+
+      if (ANALYZE) {
+        config.plugins.push(
+          new BundleAnalyzerPlugin({
+            analyzerMode: 'server',
+            analyzerPort: isServer ? 8888 : 8889,
+            openAnalyzer: true
+          })
+        );
+      }
+
+      return config;
+    }
   }
-});
+);
