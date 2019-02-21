70621ada24247c7fd468230ac5cc85d4a17c17fc
Gowtham <me@gauti.info>
2019-02-22 10:44:48 +1300

    Refactor movie styles

--- components/Movie/Movie.js
+++ components/Movie/Movie.js
@@ -111,6 +111,9 @@ class Movie extends Component {
     similarArray.forEach(a => (a.media_type = movie.media_type));
     recommendedArray.forEach(a => (a.media_type = movie.media_type));
 
+    const aggregateRating = getAggregateRating(movie);
+    const aggregateRatingCount = getAggregateRatingCount(movie);
+
     const commonJsonLdAttrs = {
       name: movie.title || movie.name,
       description: movie.overview,
@@ -139,232 +142,242 @@ class Movie extends Component {
         }}
       >
         <div className="movie__container">
-          <div className="movie__poster">
-            <button
-              className="movie__poster-button"
-              onClick={() => this.showImages(this.posterButton)}
-              ref={i => (this.posterButton = i)}
-              data-title={movie.title || movie.name}
-            >
-              <Poster
-                size={135}
-                path={movie.poster_path}
-                alt={movie.title || movie.name}
-                className="movie__image"
-              />
-            </button>
-          </div>
-          <div className="movie__misc">
-            <span className="movie__type">
-              {movie.media_type === 'movie' ? movie.media_type : 'TV Show'}
-            </span>
-            {movie.adult && (
-              <span
-                className="movie__adult tooltip"
-                data-title={`Adult ${
-                  movie.media_type === 'tv' ? 'Show' : 'Movie'
-                }`}
-              >
-                <i className="fa fa-exclamation-triangle movie__adult-icon" />
-                18+
-              </span>
-            )}
-          </div>
-          <h5 className="movie__title">{movie.title || movie.name}</h5>
-          <div className="movie__vote">
-            <Votes movie={movie} />
-          </div>
-          <div className="movie__medias">
-            {movieVideosPresent && (
-              <span className="movie__videos">
-                <button
-                  className="movie__videos-button"
-                  onClick={this.showVideos}
-                  ref={v => (this.videoButton = v)}
-                >
-                  Videos
-                </button>
-              </span>
-            )}
-            {movieImagesPresent && (
-              <span className="movie__images">
-                <button
-                  className="movie__images-button"
-                  onClick={() => this.showImages(this.imageButton)}
-                  ref={i => (this.imageButton = i)}
-                >
-                  Images
-                </button>
-              </span>
-            )}
-            {movieVideosPresent && showVideos && (
-              <MediaModal
-                show={true}
-                movie={movie}
-                onClose={this.hideVideos}
-                type="videos"
-              />
-            )}
-            {movieImagesPresent && showImages && (
-              <MediaModal
-                show={true}
-                movie={movie}
-                onClose={this.hideImages}
-                type="images"
-              />
-            )}
-          </div>
-          <div className="movie__countries">
-            {countries.map(c => (
-              <span key={c.code} className="movie__country">
-                <span className="movie__country-emoji">{c.emoji}</span>
-                {c.name}
-              </span>
-            ))}
-          </div>
-          <div className="movie__misc">
-            {movie.media_type === 'movie' ? (
-              !!movie.runtime && (
-                <div className="movie__runtime tooltip" data-title="Runtime">
-                  {formatedRunTime(movie.runtime)}
-                </div>
-              )
-            ) : (
-              <div
-                className="movie__seasons-episodes tooltip"
-                data-title="Seasons & Episodes"
-              >
-                <SeasonEpisodes
-                  seasonCount={movie.number_of_seasons}
-                  episodeCount={movie.number_of_episodes}
-                  seasons={movie.seasons}
-                  movieTitle={movie.title || movie.name}
-                  onModalOpen={() => logSeasonsOpened(this.logLabel)}
-                />
-              </div>
-            )}
-            <span
-              className="movie__date tooltip"
-              data-title={
-                movie.media_type === 'movie'
-                  ? 'Released Date'
-                  : 'First Air Date'
-              }
-            >
-              {formatedDate(movie.release_date || movie.first_air_date, 'long')}
-            </span>
-          </div>
-          <div className="movie__genres">
-            {movie.genres.map(g => (
-              <span key={g.id} className="movie__genre">
-                {g.name}
-              </span>
-            ))}
-          </div>
-          <div className="movie__misc">
-            <span className="movie__status tooltip" data-title="Status">
-              {movie.status}
-            </span>
-            <span className="movie__money">
-              {movie.networks ? (
-                <span className="box-office tooltip" data-title="Network">
-                  {movie.networks.map(n => n.name).join(',')}
-                </span>
-              ) : (
-                <BoxOffice revenue={movie.revenue} />
-              )}
-            </span>
-          </div>
-          <div className="movie__misc">
-            {director ? (
-              <span
-                className="movie__director tooltip"
-                data-title="Directed By"
-              >
-                {director.name}
-              </span>
-            ) : creators ? (
-              <span className="movie__director tooltip" data-title="Created By">
-                {creators}
-              </span>
-            ) : null}
-          </div>
-          <div className="movie__awards">
-            {movie.awards && movie.awards !== 'N/A' ? (
-              <span>{movie.awards}</span>
-            ) : (
-              <span className="movie__no-info">No Awards Info</span>
-            )}
-          </div>
-          <div className="movie__production">
-            {productionInfo ? (
-              <span className="tooltip" data-title="Production Company">
-                {productionInfo}
-              </span>
-            ) : (
-              <span className="movie__no-info">No Production Company Info</span>
-            )}
-          </div>
-          <div className="movie__dvd-release-date">
-            {movie.dvdReleaseDate && movie.dvdReleaseDate !== 'N/A' ? (
-              <span className="tooltip" data-title="DVD Release Date">
-                {formatedDate(movie.dvdReleaseDate, 'long')}
-              </span>
-            ) : (
-              <span className="movie__no-info">No DVD Release Date</span>
-            )}
-          </div>
-          <div className="movie__languages">
-            <Languages
-              translations={movie.translations.translations}
-              movieTitle={movie.title || movie.name}
-              onModalOpen={() => logLanguagesOpened(this.logLabel)}
-              orginalLanguageCode={movie.original_language}
+          <button
+            className="movie__poster"
+            onClick={() => this.showImages(this.posterButton)}
+            ref={i => (this.posterButton = i)}
+            data-title={movie.title || movie.name}
+          >
+            <Poster
+              size={135}
+              path={movie.poster_path}
+              alt={movie.title || movie.name}
+              className="movie__image"
             />
-          </div>
-          <div className="movie__related">
-            <RelatedContainer
-              related={mergeUniqMovieArrays(recommendedArray, similarArray)}
-              movieTitle={movie.title || movie.name}
-              movieType={movie.media_type}
-              onModalOpen={() => logRelatedOpened(this.logLabel)}
-            />
-          </div>
-          <div className="movie__overview">
-            <ShowMoreText
-              onClick={() => logOverviewOpened(this.logLabel)}
-              anchorClass="movie__more-link"
-              more="more"
-              less={false}
-            >
-              {movie.overview}
-            </ShowMoreText>
-          </div>
-
-          <div className="movie__cast-crew">
-            {movie.credits.cast.length > 1 && (
-              <Credits
-                credits={movie.credits.cast}
-                movieTitle={movie.title || movie.name}
-                type="cast"
-                onModalOpen={() => logCastOpened(this.logLabel)}
-              />
-            )}
-          </div>
-          <div className="movie__cast-crew">
-            {movie.credits.crew.length > 1 && (
-              <Credits
-                credits={movie.credits.crew}
-                movieTitle={movie.title || movie.name}
-                type="crew"
-                onModalOpen={() => logCrewOpened(this.logLabel)}
-              />
-            )}
-          </div>
-          <button onClick={removeMovie} className="movie__remove">
-            &times;
           </button>
         </div>
+
+        <div className="movie__container movie__container--xs">
+          <span className="movie__type">
+            {movie.media_type === 'movie' ? movie.media_type : 'TV Show'}
+          </span>
+          {movie.adult && (
+            <span
+              className="movie__adult tooltip"
+              data-title={`Adult ${
+                movie.media_type === 'tv' ? 'Show' : 'Movie'
+              }`}
+            >
+              <i className="fa fa-exclamation-triangle movie__adult-icon" />
+              18+
+            </span>
+          )}
+        </div>
+
+        <div className="movie__container">
+          <h5 className="movie__title">{movie.title || movie.name}</h5>
+        </div>
+
+        <div className="movie__vote movie__container">
+          <Votes movie={movie} />
+        </div>
+
+        <div className="movie__medias movie__container">
+          {movieVideosPresent && (
+            <span className="movie__videos">
+              <button
+                className="movie__videos-button"
+                onClick={this.showVideos}
+                ref={v => (this.videoButton = v)}
+              >
+                Videos
+              </button>
+            </span>
+          )}
+          {movieImagesPresent && (
+            <span className="movie__images">
+              <button
+                className="movie__images-button"
+                onClick={() => this.showImages(this.imageButton)}
+                ref={i => (this.imageButton = i)}
+              >
+                Images
+              </button>
+            </span>
+          )}
+          {movieVideosPresent && showVideos && (
+            <MediaModal
+              show={true}
+              movie={movie}
+              onClose={this.hideVideos}
+              type="videos"
+            />
+          )}
+          {movieImagesPresent && showImages && (
+            <MediaModal
+              show={true}
+              movie={movie}
+              onClose={this.hideImages}
+              type="images"
+            />
+          )}
+        </div>
+
+        <div className="movie__misc movie__countries movie__container">
+          {countries.map(c => (
+            <span key={c.code} className="movie__country">
+              <span className="movie__country-emoji">{c.emoji}</span>
+              {c.name}
+            </span>
+          ))}
+        </div>
+
+        <div className="movie__misc movie__misc--centered movie__container">
+          {movie.media_type === 'movie' ? (
+            !!movie.runtime && (
+              <span className="movie__runtime tooltip" data-title="Runtime">
+                {formatedRunTime(movie.runtime)}
+              </span>
+            )
+          ) : (
+            <span
+              className="movie__seasons-episodes tooltip"
+              data-title="Seasons & Episodes"
+            >
+              <SeasonEpisodes
+                seasonCount={movie.number_of_seasons}
+                episodeCount={movie.number_of_episodes}
+                seasons={movie.seasons}
+                movieTitle={movie.title || movie.name}
+                onModalOpen={() => logSeasonsOpened(this.logLabel)}
+              />
+            </span>
+          )}
+          <span
+            className="movie__date tooltip"
+            data-title={
+              movie.media_type === 'movie' ? 'Released Date' : 'First Air Date'
+            }
+          >
+            {formatedDate(movie.release_date || movie.first_air_date, 'long')}
+          </span>
+        </div>
+
+        <div className="movie__misc movie__genres movie__container">
+          {movie.genres.map(g => (
+            <span key={g.id} className="movie__genre">
+              {g.name}
+            </span>
+          ))}
+        </div>
+
+        <div className="movie__misc movie__misc--centered movie__container">
+          <span className="movie__status tooltip" data-title="Status">
+            {movie.status}
+          </span>
+          {movie.networks ? (
+            <span className="movie__network tooltip" data-title="Network">
+              {movie.networks.map(n => n.name).join(',')}
+            </span>
+          ) : (
+            <BoxOffice revenue={movie.revenue} />
+          )}
+        </div>
+
+        <div className="movie__misc movie__container">
+          {director ? (
+            <span className="movie__director tooltip" data-title="Directed By">
+              {director.name}
+            </span>
+          ) : creators ? (
+            <span className="movie__director tooltip" data-title="Created By">
+              {creators}
+            </span>
+          ) : null}
+        </div>
+
+        <div className="movie__misc movie__awards movie__container">
+          {movie.awards && movie.awards !== 'N/A' ? (
+            <span>{movie.awards}</span>
+          ) : (
+            <span className="movie__no-info">No Awards Info</span>
+          )}
+        </div>
+
+        <div className="movie__misc movie__production movie__container">
+          {productionInfo ? (
+            <span className="tooltip" data-title="Production Company">
+              {productionInfo}
+            </span>
+          ) : (
+            <span className="movie__no-info">No Production Company Info</span>
+          )}
+        </div>
+
+        <div className="movie__misc movie__dvd-release-date movie__container">
+          {movie.dvdReleaseDate && movie.dvdReleaseDate !== 'N/A' ? (
+            <span className="tooltip" data-title="DVD Release Date">
+              {formatedDate(movie.dvdReleaseDate, 'long')}
+            </span>
+          ) : (
+            <span className="movie__no-info">No DVD Release Date</span>
+          )}
+        </div>
+
+        <div className="movie__languages movie__container">
+          <Languages
+            translations={movie.translations.translations}
+            movieTitle={movie.title || movie.name}
+            onModalOpen={() => logLanguagesOpened(this.logLabel)}
+            orginalLanguageCode={movie.original_language}
+          />
+        </div>
+
+        <div className="movie__related movie__container">
+          <RelatedContainer
+            related={mergeUniqMovieArrays(recommendedArray, similarArray)}
+            movieTitle={movie.title || movie.name}
+            movieType={movie.media_type}
+            onModalOpen={() => logRelatedOpened(this.logLabel)}
+          />
+        </div>
+
+        <div className="movie__overview movie__container">
+          <ShowMoreText
+            onClick={() => logOverviewOpened(this.logLabel)}
+            anchorClass="movie__more-link"
+            more="more"
+            less={false}
+          >
+            {movie.overview}
+          </ShowMoreText>
+        </div>
+
+        <div className="movie__cast-crew movie__container">
+          {movie.credits.cast.length > 1 && (
+            <Credits
+              credits={movie.credits.cast}
+              movieTitle={movie.title || movie.name}
+              type="cast"
+              onModalOpen={() => logCastOpened(this.logLabel)}
+            />
+          )}
+        </div>
+
+        <div className="movie__cast-crew movie__container">
+          {movie.credits.crew.length > 1 && (
+            <Credits
+              credits={movie.credits.crew}
+              movieTitle={movie.title || movie.name}
+              type="crew"
+              onModalOpen={() => logCrewOpened(this.logLabel)}
+            />
+          )}
+        </div>
+
+        <button onClick={removeMovie} className="movie__remove">
+          &times;
+        </button>
 
         <JSONLD>
           {movie.media_type === 'movie' ? (
@@ -381,8 +394,8 @@ class Movie extends Component {
                 jsonldtype="AggregateRating"
                 schema={{
                   bestRating: 10,
-                  ratingValue: getAggregateRating(movie),
-                  ratingCount: getAggregateRatingCount(movie)
+                  ratingValue: aggregateRating,
+                  ratingCount: aggregateRatingCount
                 }}
               />
               <Generic
@@ -421,8 +434,8 @@ class Movie extends Component {
                 jsonldtype="AggregateRating"
                 schema={{
                   bestRating: 10,
-                  ratingValue: getAggregateRating(movie),
-                  ratingCount: getAggregateRatingCount(movie)
+                  ratingValue: aggregateRating,
+                  ratingCount: aggregateRatingCount
                 }}
               />
               <Generic
