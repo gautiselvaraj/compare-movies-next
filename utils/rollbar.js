4b47a02f08955d47cddbeb2a17cfc3c027a20c5d
Gowtham <me@gauti.info>
2019-02-21 21:09:00 +1300

    Fix rollbar typo

--- utils/rollbar.js
+++ utils/rollbar.js
@@ -1,4 +1,4 @@
-const client = typeof window !== 'undefined';
+const isClient = typeof window !== 'undefined';
 
 export default error =>
   isClient && typeof Rollbar !== 'undefined' && Rollbar.error(error);
