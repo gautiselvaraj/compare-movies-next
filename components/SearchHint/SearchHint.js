1bdf6caf5d66060dae39c585d48c24da9d0205e9
Gowtham <me@gauti.info>
2019-02-22 18:12:53 +1300

    Update SearchHint.js

--- components/SearchHint/SearchHint.js
+++ components/SearchHint/SearchHint.js
@@ -6,8 +6,7 @@ export default () => (
   <div className="search-hint">
     <div className="search-hint__text">
       Type any
-      <RotateText children={['Movie', 'TV Show']} avgTypingDelay={100} />
-      title...
+      <RotateText children={['Movie...', 'TV Show...']} avgTypingDelay={100} />
     </div>
   </div>
 );
